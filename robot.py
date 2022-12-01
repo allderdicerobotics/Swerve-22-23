1297e269e2b8c74753f719b209552d547ec3a267
allderdicerobotics <35245530+allderdicerobotics@users.noreply.github.com>
2022-12-01 15:26:57 -0500

    Made the files

---
+++ robot.py
@@ -0,0 +1,11 @@
+import wpilib
+from magicbot import MagicRobot
+
+# Main robot class
+class FRCRobot(MagicRobot):
+    def createObjects(self):
+        pass
+
+# Main section
+if __name__ == "__main__":
+    wpilib.run(FRCRobot)
