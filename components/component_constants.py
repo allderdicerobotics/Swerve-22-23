a9c5e2d72510a9cba26d64e31d97d7eb61fc3d0c
allderdicerobotics <35245530+allderdicerobotics@users.noreply.github.com>
2022-12-06 14:51:45 -0500

    drive properties stuff

--- components/component_constants.py
+++ components/component_constants.py
@@ -2,7 +2,9 @@
 
 class Encoders:
     STD_VOLTAGE_MAX = 5.0
+
 class DriveProperties:
+    CHASSIS_DIMS = (1.0, 1.0)
     kWheelRadius = 0.0508
     kDriveWheelGearRatio = 6.55
     
--- components/swervedrive.py
+++ components/swervedrive.py
@@ -9,9 +9,18 @@ import rev
 
 import component_constants as c
 
+class SwerveControlT(t.TypedDict):
+    """
+    A type representing a dictionary with the values that define what
+    we want the swerve drive to do.
+    """
+    fwd: float          # [-1, 1]
+    strafe: float
+    rcw: float
+
 class SwerveDrive:
 
-    # All of swerve modules
+    # All of swerve modules (to be injected)
     swerveModuleFL: SwerveModule
     swerveModuleFR: SwerveModule
     swerveModuleBL: SwerveModule
@@ -21,7 +30,10 @@ class SwerveDrive:
     swerveModules: t.List[SwerveModule]
 
     # The dimensions of the chassis
-    chassisDims: t.Tuple[float, float] = c.Swerve
+    chassisDims: t.Tuple[float, float] = c.DriveProperties.CHASSIS_DIMS
+
+    # The intended vectors for controlling the swerve modules
+    controlIntent: t.
 
     def setup(self):
         self.swerveModules = [
@@ -30,4 +42,5 @@ class SwerveDrive:
             self.swerveModuleBL,
             self.swerveModuleBR,
         ]
+
 
