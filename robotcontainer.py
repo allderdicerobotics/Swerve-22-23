c4a84bd8a5bba81a9a619b8f1497261f64cd467f
allderdicerobotics <35245530+allderdicerobotics@users.noreply.github.com>
2022-12-06 16:16:58 -0500

    no copy joystick from ronmi

--- robotcontainer.py
+++ robotcontainer.py
@@ -5,9 +5,3 @@ class RobotContainer:
     def __init__(self) -> None:
         self.drive = swervedrive()
         self.controller = wpilib.Joystick(0)
-    def getDriveCommand(self) -> swervedrive:
-        return swervedrive(
-            self.drive,
-            lambda: self.controller.getRawAxis(0),
-            lambda: self.controller.getRawAxis(1),
-        )
