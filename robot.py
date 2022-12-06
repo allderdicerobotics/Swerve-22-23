29769d81da943cd7beef9d64ebb65c3fedcd25ed
allderdicerobotics <35245530+allderdicerobotics@users.noreply.github.com>
2022-12-06 16:19:47 -0500

    Update robot.py

--- robot.py
+++ robot.py
@@ -1,13 +1,25 @@
 import wpilib
 from magicbot import MagicRobot
 from robotcontainer import RobotContainer
+from swervedrive import SwerveDrive
+from swervemodule import SwerveModule
+
 # Main robot class
 class FRCRobot(MagicRobot):
-    def robotInit(self) -> None:
-        self.container = RobotContainer()
-    
+    drivetrain: SwerveDrive
+
+    drivetrain_swerveModuleFL: SwerveModule
+    drivetrain_swerveModuleFR: SwerveModule
+    drivetrain_swerveModuleBL: SwerveModule
+    drivetrain_swerveModuleBR: SwerveModule
+
     def createObjects(self):
-        pass
+        self.drivetrain = SwerveDrive()
+        self.drivetrain_swerveModuleFL = ...
+        self.drivetrain_swerveModuleFR = ...
+        self.drivetrain_swerveModuleBL = ...
+        self.drivetrain_swerveModuleBR = ...
+        
 
 # Main section
 if __name__ == "__main__":
