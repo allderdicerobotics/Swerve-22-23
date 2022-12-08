855ba1b179ae9e8b3ea3b993c66cdeaa285a929b
allderdicerobotics <35245530+allderdicerobotics@users.noreply.github.com>
2022-12-08 15:53:07 -0500

    Add constants for swerve ports that I think are right

--- outer_constants.py
+++ outer_constants.py
@@ -1,26 +1,28 @@
 # Constants for anything other than components
 
+from wpilib import AnalogInput
+
 # Hardware
 class Hw:
     swerve_cfg = {
         "FL": {
-            "driveMotor": ...,
-            "turningMotor": ...,
-            "turningEncoder": ...,
+            "driveMotor": (5,),
+            "turningMotor": (6,),
+            "turningEncoder": { "devInput": AnalogInput(0) },
         },
         "FR": {
-            "driveMotor": ...,
-            "turningMotor": ...,
-            "turningEncoder": ...,
+            "driveMotor": (3,),
+            "turningMotor": (4,),
+            "turningEncoder": { "devInput": AnalogInput(2) },
         },
         "BL": {
-            "driveMotor": ...,
-            "turningMotor": ...,
-            "turningEncoder": ...,
+            "driveMotor": (10,),
+            "turningMotor": (9,),
+            "turningEncoder": { "devInput": AnalogInput(1) },
         },
         "BR": {
-            "driveMotor": ...,
-            "turningMotor": ...,
-            "turningEncoder": ...,
+            "driveMotor": (12,),
+            "turningMotor": (11,),
+            "turningEncoder": { "devInput": AnalogInput(3) },
         },
     }
