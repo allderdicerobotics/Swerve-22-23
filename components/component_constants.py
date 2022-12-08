ebcc7ce887cb9e762326dd959bcb9582250aa3cc
allderdicerobotics <35245530+allderdicerobotics@users.noreply.github.com>
2022-12-08 15:48:04 -0500

    Fix dimensions in `component_constants.py`

--- components/component_constants.py
+++ components/component_constants.py
@@ -4,7 +4,7 @@ class Encoders:
     STD_VOLTAGE_MAX = 5.0
 
 class DriveProperties:
-    CHASSIS_DIMS = (1.0, 1.0)
+    CHASSIS_DIMS = (0.762, 0.762)
     WHEEL_RADIUS = 0.0508
     DRIVE_WHEEL_GEAR_RATIO = 6.55
     MODULE_MAX_ANGULAR_VELOCITY = 1800
