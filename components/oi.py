0cf3936e92e48facd39dfccb43b589f40fca9732
allderdicerobotics <35245530+allderdicerobotics@users.noreply.github.com>
2022-12-13 15:06:03 -0500

    Update oi.py

--- components/oi.py
+++ components/oi.py
@@ -5,11 +5,13 @@ class OI:
     driver: Joystick
 
     def get_swerve_intent(self):
-        return SwerveControl(
+        control = SwerveControl(
             fwd = self.driver.getRawAxis(0),
             strafe = self.driver.getRawAxis(1),
             rcw = 0.0,
         )
+        print(f"intent: {control}")
+        return control
     
     def execute(self):
         pass
